6fffce9a24d16ddf5ed3a3d7febe6d3160ffeb6d
svh <nwahs007@gmail.com>
2024-03-24 15:06:32 -0700

    Initial commit + BinaryTree classes

---
+++ app/src/main/java/com/example/visualization_binarytree/BinaryTree.java
@@ -0,0 +1,77 @@
+package com.example.visualization_binarytree;//import java.util.ArrayList;
+
+public class BinaryTree
+{
+    // variables
+    private final Node root;
+
+    // constructor
+    public BinaryTree(int data)
+    {
+        root = new Node(data);
+    }
+    public BinaryTree(int[] data)
+    {
+        root = new Node();
+        for(int n: data) {
+            this.addData(n);
+        }
+    }
+    public BinaryTree()
+    {
+        root = new Node();
+    }
+
+    // getter and setter
+    public Node getRoot()
+    {
+        return root;
+    }
+    public void setRoot(int data)
+    {
+        root.setData(data);
+    }
+
+    // brain methods
+    public int treeLeaf() {
+        return root.leafCount();
+    }
+    public void addData(int data)
+    {
+        if(root.getData() != null) root.addNew(data);
+        else root.setData(data);
+    }
+    public void addData(int[] data)
+    {
+        for(int n: data) {
+            if(root.getData() != null) root.addNew(n);
+            else root.setData(n);
+        }
+    }
+
+    // output formatted data
+    public String toString()
+    {
+        String out = root.printBranches();
+        if(out.equals(""))
+            return "Tree yet to be filled.";
+        else {
+            out = out.substring(0,out.length()-1);
+            return "[" + out.replaceAll(" ", ", ") + "]";
+        }
+    }
+    public int[] toArray()
+    {
+        String out = root.printBranches();
+        if(out == "")
+            return null;
+        String[] outArr = out.split(" ");
+        int nodes = outArr.length;
+        int[] array = new int[nodes];
+        for(int i = 0; i < nodes; i++)
+        {
+            array[i] = Integer.parseInt(outArr[i]);
+        }
+        return array;
+    }
+}
---
+++ app/src/main/java/com/example/visualization_binarytree/Node.java
@@ -0,0 +1,136 @@
+package com.example.visualization_binarytree;
+
+public class Node
+{
+    // instance variables
+    private Integer data;
+    private Node left;
+    private Node right;
+    private Node parent;
+
+    // constructors
+    public Node(int data) //used to create root
+    {
+        this.data = data;
+        left = null;
+        right = null;
+        parent = null;
+    }
+    public Node(int data, Node parent) //used to create new nodes
+    {
+        this.data = data;
+        left = null;
+        right = null;
+        this.parent = parent;
+    }
+    public Node() //used to create empty root
+    {
+        data = null;
+        left = null;
+        right = null;
+        parent = null;
+    }
+
+    // getters and setters
+    public void setData(int data)
+    {
+        this.data = data;
+    }
+    public Integer getData() //try catch to avoid NullPointerException
+    {
+        try {
+            return data;
+        }
+        catch(NullPointerException e) {
+            return null;
+        }
+    }
+    public Node getRight() {
+        return right;
+    }
+    public Node getParent()
+    {
+        return parent;
+    }
+// [LEFT UNREMOVED JUST IN CASE]
+//    public void setLeft(int data)
+//    {
+//        left.setData(data);
+//    }
+//    public void setRight(int data) {
+//        right.setData(data);
+//    }
+//    public com.example.com.example.BinaryTree.BinaryTree.Node getLeft() {
+//        return left;
+//    }
+
+//    public void setParent(int data)
+//    {
+//        parent.setData(data);
+//    }
+
+    // brain methods
+    public int findHeight() //finds the distance to root node
+    {
+        int count = 0;
+        Node check = this;
+        while(check.getParent() != null)
+        {
+            count++;
+            check = check.getParent();
+        }
+        return count;
+    }
+    //    [RECURSIVE VERSION of findHeight()]
+//    public int findHeight() //finds the distance to root node, recursively
+//    {
+//        if(parent == null) return 0;
+//        return 1 + parent.findHeight();
+//    }
+    private Node findNext() //finds the left most node, will keep it as a complete binary tree
+    {
+        if(right != null && left != null)
+        {
+            Node sub1 = left.findNext();
+            Node sub2 = right.findNext();
+            if(sub1.findHeight() > sub2.findHeight())
+            {
+                if(sub1.parent.getRight() == null) return sub1;
+                return sub2;
+            }
+            else if(sub1.findHeight() < sub2.findHeight())
+            {
+                if(sub2.parent.getRight() == null) return sub2;
+                return sub1;
+            }
+            else return sub1;
+        }
+        else return this;
+    }
+    public void addNew(int addData)
+    {
+        Node addLeaf = this.findNext();
+        if(addLeaf.left != null) addLeaf.right = new Node(addData, addLeaf);
+        else addLeaf.left = new Node(addData, addLeaf);
+    }
+    private boolean isLeaf() //leafs will have two empty child references
+    {
+        return left == null && right == null;
+    }
+    public int leafCount() //1 leaf or 2 leaf or 0 leaf
+    {
+        int count = 0;
+        if(this.isLeaf()) return 1;
+        if(left != null) count += left.leafCount();
+        if(right != null) count += right.leafCount();
+        return count;
+    }
+    public String printBranches() //print the branches with inorder traversal
+    {
+        if(data == null) return "";
+        String output = data + " ";
+        if(left != null) output = left.printBranches() + output;
+        if(right != null) output = output + right.printBranches();
+        return output;
+    }
+}
